5cc3e445f73ad38a3a10167d89863854dab2fcb9
Cameron Kerr <cameron.kerr.nz@gmail.com>
2023-08-30 17:23:00 +1200

    initial commit

---
+++ demo/demo.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python-2.7
+
+import logging
+
+if __name__ == "__main__":
+
+    logging.basicConfig(filename='demo.log',level=logging.DEBUG)
+
---
+++ demo/test_demo.py
@@ -0,0 +1,7 @@
+import unittest
+import demo
+
+class TestDemo(unittest.TestCase):
+
+    def test_demo(self):
+        self.assertTrue(True)
